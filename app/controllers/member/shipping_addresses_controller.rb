5da8e6863c0fcb61e64098cfdf5749bbaac28204
vagrant <vagrant@localhost.localdomain>
2020-09-05 06:01:41 +0000

    add-members-shipping

--- app/controllers/member/shipping_addresses_controller.rb
+++ app/controllers/member/shipping_addresses_controller.rb
@@ -1,7 +1,26 @@
 class Member::ShippingAddressesController < ApplicationController
   def index
+  	@address = ShippingAddress.new
+  	@addresses = ShippingAddress.all
+  end
+
+
+  def create
+  	@address = ShippingAddress.new(address_params)
+  	@address.member_id = current_member.id
+  	if @address.save
+  	else
+  	@addresses = ShippingAddress.all
+  	render :index
+    end
   end
 
   def edit
   end
+
+  private
+  def address_params
+  	paramas.require(:shipping_address).permit(:member_id, :name, :postal_code, :address)
+  end
+
 end
