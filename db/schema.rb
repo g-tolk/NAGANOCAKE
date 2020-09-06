3466538c9197fb0342829d4bdf94b643bc49ef28
vagrant <vagrant@localhost.localdomain>
2020-09-06 08:12:13 +0000

    add-migration

--- db/schema.rb
+++ db/schema.rb
@@ -73,7 +73,7 @@ ActiveRecord::Schema.define(version: 2020_08_31_081954) do
     t.integer "member_id", null: false
     t.integer "postage", null: false
     t.integer "payment_amount", null: false
-    t.integer "payment_method", null: false
+    t.boolean "payment_method", default: true, null: false
     t.integer "order_status", null: false
     t.string "receiver", null: false
     t.string "postal_code", null: false
