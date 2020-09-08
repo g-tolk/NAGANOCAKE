fcb0cb28a96e6b415ef4d8da54327d134c571496
rie <kou.kotetsu@gmail.com>
2020-09-08 12:04:28 +0900

    cange

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,8 +2,8 @@ class Product < ApplicationRecord
   attachment :image
 	has_many :cart_products, dependent: :destroy
 	has_many :order_products, dependent: :destroy
+	belongs_to :genre
 
-  validates :genre_id, presence: true
-
-  belongs_to :genre
+	validates :name, presence: true, uniqueness: true
+  	validates :genre_id, presence: true
 end
