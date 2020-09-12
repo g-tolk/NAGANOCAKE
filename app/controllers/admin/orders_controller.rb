ea71fd26e3329c33dc26df044c85fb4bf3d13472
rie <kou.kotetsu@gmail.com>
2020-09-12 16:57:24 +0900

    change

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -9,26 +9,28 @@ class Admin::OrdersController < ApplicationController
 
   def update
      @order = Order.find(params[:id])
-    if @order.update(order_params)&& (params[:order_status] == "0")
+     @order.update(order_params)
+     #@order.update(order_params)
+      #&& (params[:order_status] == "0")
       redirect_to root_path
        OrderProduct.product_status = 1
        OrderProduct.update
       #redirect_to admin_order_path(@order)
-    elsif
-      @order.update(order_params) && (enum product_status == 2)
-      enum order_status = 2.to_s.update
-      redirect_to admin_order_path(@order)
-    elsif
-      @order.update(order_params) && (enum product_status == 3)
-      enum order_status = 3.to_s.update
-      redirect_to admin_order_path(@order)
-    end
     redirect_to admin_order_path(@order)
   end
 
   private
+
   def order_params
-  params.require(:order).permit(order_status: params[:order_status])
+    order_params.each do |key,value|
+      if integer_string?(value)
+         order_params[key]=value.to_i
+      end
+    end
   end
+
+  #def order_params
+  #params.require(:order).permit(:order_status)
+  #end
 end
 #params[:address][:prefecture].to_i
